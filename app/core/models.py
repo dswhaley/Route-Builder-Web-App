ceba750f8f373b6ca385ed23ab2e0dbb60f0a5ab
HANCOCKOR21 <hancockor21@gcc.edu>
2025-11-10 22:02:18 -0500

    Added a test user in the core model

--- app/core/models.py
+++ app/core/models.py
@@ -67,45 +67,18 @@ def init_app_db():
     db.drop_all()
     db.create_all()
     # create a testing account at app launch
-    admin = User(email='test@gcc.edu', password='reallygoodpassword') # type: ignore
-    db.session.add(admin)
-    # create some initial example data
-    # Create bank records to be inserted
-    bank1 = Bank(code="GB1", name="Grove City Bank1",                      # type: ignore[call-arg]
-                address="225 bank drive, grove city pa")                   # type: ignore[call-arg]
-    bank2 = Bank(code="GB2", name="Grove City Bank2",                      # type: ignore[call-arg]
-                address="555 other rd, grove city pa")                     # type: ignore[call-arg]
-    bank3 = Bank(code="GB3", name="PGH bank",                              # type: ignore[call-arg]
-                address="111 city rd, pittsburgh, pa")                     # type: ignore[call-arg]
-
-    # Create customer records to be inserted
-    cust1 = Customer(c_number=1111, name="John Smith", phone="5552223456", # type: ignore[call-arg]
-                email="john.smith@gmail.com", membership='G',              # type: ignore[call-arg]
-                address="234 middle st, grove city, pa")                   # type: ignore[call-arg]
-    cust2 = Customer(c_number=2222, name="Joe Smith", phone="5551113456",  # type: ignore[call-arg]
-                email="joe.smith@gmail.com", membership='S',               # type: ignore[call-arg]
-                address="234 middle st, grove city, pa")                   # type: ignore[call-arg]
-    cust3 = Customer(c_number=3333, name="Jane Brown", phone="5553337890", # type: ignore[call-arg]
-            email="jane.brown@gmail.com", membership='S',                  # type: ignore[call-arg]
-            address="222 center ave, pittsburgh, pa")                      # type: ignore[call-arg]
-    cust4 = Customer(c_number=4444, name="John Brown", phone="5553337890", # type: ignore[call-arg]
-                email="john.brown@gmail.com",                              # type: ignore[call-arg]
-                address="222 center ave, pittsburgh, pa")                  # type: ignore[call-arg]
-
-    # Create account records to be inserted
-    acct1 = Account(customer=cust1, bank=bank1, account_no=121234345609,   # type: ignore[call-arg]
-                startdate=date(2005, 6, 1), balance=500.5)                 # type: ignore[call-arg]
-    acct2 = Account(customer=cust1, bank=bank1, account_no=121234345610,   # type: ignore[call-arg]
-                startdate=date(2007, 8, 29), balance=200.5)                # type: ignore[call-arg]
-    acct3 = Account(customer=cust2, bank=bank2, account_no=121234345611,   # type: ignore[call-arg]
-                startdate=date(2010, 7, 11), balance=100.5)                # type: ignore[call-arg]
-    acct4 = Account(customer=cust3, bank=bank3, account_no=121234345612,   # type: ignore[call-arg]
-                startdate=date(2003, 10, 1), balance=1000.5)               # type: ignore[call-arg]
-    acct5 = Account(customer=cust3, bank=bank1, account_no=121234345613,   # type: ignore[call-arg]
-                startdate=date(2011, 11, 11), balance=50)                  # type: ignore[call-arg]
-
-    # Add all of these records to the session and commit changes
-    db.session.add_all((bank1, bank2, bank3, cust1, cust2, cust3, cust4, 
-        acct1, acct2, acct3, acct4, acct5))
+    example_user = User(
+        email="huckfinn@example.com",               # type: ignore[call-arg]
+        password_hash='reallygoodpassword',         # type: ignore[call-arg]
+        dob=date(2004, 12, 16),                     # type: ignore[call-arg]
+        admin=False,                                # type: ignore[call-arg]
+        username="huckfinn")                        # type: ignore[call-arg]
+    db.session.add(example_user)                
     db.session.commit()
-    db.session.commit()
+
+    user = User.query.filter_by(email="huckfinn@example.com").first()
+
+    if user:
+        print("✅ User found:", user.username)
+    else:
+        print("❌ No user with that email.")
