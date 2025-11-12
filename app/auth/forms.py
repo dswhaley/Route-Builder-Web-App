4f6979c38475259492783e720a8e6c24134b3a21
HANCOCKOR21 <hancockor21@gcc.edu>
2025-11-11 21:11:49 -0500

    Add SignUpForm to for users

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import EmailField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Email, Length
+from wtforms import EmailField, PasswordField, SubmitField, StringField, DateField
+from wtforms.validators import InputRequired, Email, Length, DataRequired
 
 MIN_PASSWORD_LENGTH: int = 8
 
@@ -10,3 +10,25 @@ class LoginForm(FlaskForm):
     password: PasswordField = PasswordField('Password',
         validators=[InputRequired(), Length(min=MIN_PASSWORD_LENGTH)])
     submit: SubmitField = SubmitField("Login")
+
+class SignUpForm(FlaskForm):
+    username: StringField = StringField(
+        'Username', 
+        validators=[
+            DataRequired(message="Please enter a username."),
+            Length(min=3, max=20, message="Username must be between 3 and 20 characters.")
+        ]
+    )
+    dob: DateField = DateField(
+        'Date of Birth',
+        format='%Y-%m-%d',
+        validators=[DataRequired(message="Please enter your date of birth.")]
+    )
+    email: EmailField = EmailField('Email',
+        validators=[InputRequired(), Email()])
+    
+    password: PasswordField = PasswordField('Password',
+        validators=[InputRequired(), Length(min=MIN_PASSWORD_LENGTH)])
+    
+    confirm_password: PasswordField = PasswordField('Password',
+        validators=[InputRequired(), Length(min=MIN_PASSWORD_LENGTH)])
