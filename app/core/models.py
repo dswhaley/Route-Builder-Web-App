67d93f5e438320a6ed9f766fc3598f1e61f62c70
dswhaley <whaleyds22@gcc.edu>
2025-11-14 13:15:16 -0500

    Added distance col to Activity

--- app/core/models.py
+++ app/core/models.py
@@ -28,6 +28,7 @@ class Activity(db.Model):
     description:        Mapped[str] = mapped_column(nullable=True)
     start_time:         Mapped[datetime] = mapped_column(nullable=False)
     type:               Mapped[Type] = mapped_column(nullable=False)
+    distance:           Mapped[float] = mapped_column(nullable=False)
     duration_minute:   Mapped[int] = mapped_column(nullable=False)
     user:               Mapped['User'] = relationship()
     route:               Mapped['Route'] = relationship()
@@ -39,7 +40,7 @@ class Route(db.Model):
     distance: Mapped[float] = mapped_column(nullable=False)
     elevation: Mapped[float] = mapped_column(nullable=False)
     route_name: Mapped[str] = mapped_column(nullable=False)
-
+    coord_string: Mapped[str] = mapped_column(nullable=False)
     #Storing images in a database is not a good idea as they take up a lot of space.
     #Instead image_name stores the id of the route, and the image will be saved in route_images.
     #That way when the image is needed it can be fetched from that directory by that name. 
@@ -80,9 +81,9 @@ def init_app_db():
 
     # --- ROUTES ---
     routes = [
-        Route(route_name="Route 1", distance=5.0, elevation=10, image_name="1.png"),   # type: ignore[call-arg]
-        Route(route_name="Route 2", distance=10.0, elevation=50, image_name="2.png"),  # type: ignore[call-arg]
-        Route(route_name="Route 3", distance=15.0, elevation=100, image_name="3.png"), # type: ignore[call-arg]
+        Route(route_name="Route 1", distance=5.0, elevation=10, coord_string = "", image_name="1.png"),   # type: ignore[call-arg]
+        Route(route_name="Route 2", distance=10.0, elevation=50, coord_string = "", image_name="2.png"),  # type: ignore[call-arg]
+        Route(route_name="Route 3", distance=15.0, elevation=100, coord_string = "", image_name="3.png"), # type: ignore[call-arg]
     ]
     db.session.add_all(routes)
     db.session.commit()
@@ -96,8 +97,7 @@ def init_app_db():
         Activity(user_id=3, route_id=3, title="Run 2", description="another test", # type: ignore[call-arg]
                  start_time=datetime(2025, 11, 12, 7, 0), type=Type.RUN, duration_minute=20), # type: ignore[call-arg]
         Activity(user_id=2, title="Run 3", description="another test", # type: ignore[call-arg]
-                 start_time=datetime(2025, 11, 5, 7, 0), type=Type.RUN, duration_minute=20), # type: ignore[call-arg]
-        
+                 start_time=datetime(2025, 11, 5, 7, 0), type=Type.RUN, duration_minute=20), # type: ignore[call-arg]     
     ]
     
     db.session.add_all(activities)
