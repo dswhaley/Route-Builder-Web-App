0f865b6564d9d53a888d735488df9248af4a18b9
JMWolverine <147189390+JMWolverine@users.noreply.github.com>
2025-12-09 14:03:52 -0500

    working on typeScript

--- app/static/scripts/doomScroll.ts
+++ app/static/scripts/doomScroll.ts
@@ -1,3 +1,9 @@
+interface Activity {
+    title: string;
+    distance: number;
+    duration_minute: number;
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
     // find the button and add an event listener
     const btn: HTMLButtonElement = <HTMLButtonElement> document.getElementById("loadMore");
@@ -9,10 +15,11 @@ function addMore(){
     const newActivities = document.getElementsByTagName("ol");
     //loop through the new activites
     const c = <HTMLDivElement> document.getElementById("card1");
+    const i = document.getElementById("row");
     for(const a of newActivities){
-        let n = <HTMLDivElement> c.cloneNode();
+        let n= document.createElement("div");
         let d = helper(n, a);
-        document.append(d);
+        i.append(d);
     }
     
     
@@ -21,6 +28,15 @@ function addMore(){
 }
 
 function helper(n : HTMLDivElement, a: HTMLOListElement){
-    n.innerText = a.innerText
+    n.classList.add("col-sm-12 col-md-6 col-lg-6 col-xl-4");
+    let b = document.createElement("div");
+    b.classList.add("cardEx");
+    n.appendChild(b);
+    let c = document.createElement("div");
+    c.classList.add("card");
+    c.classList.add("text-center");
+    b.appendChild(c);
+    let d = document.createElement("div")
+    d.classList.add("card-header");
     return n;
 }
