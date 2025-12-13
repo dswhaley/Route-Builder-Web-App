b5b79226588638329cb2fd2c62dffaaffce5054b
dswhaley <whaleyds22@gcc.edu>
2025-12-13 10:50:31 -0500

    Not quite done but didn't want merge conflicts

--- app/static/scripts/modal.ts
+++ app/static/scripts/modal.ts
@@ -1,9 +1,23 @@
 document.addEventListener("click", activateModal);
 
-function activateModal(event: MouseEvent){
+async function activateModal(event: MouseEvent){
   
     const modal = <HTMLInputElement> document.getElementById("create-activity-modal");
     const modalInputsDiv = <HTMLElement> document.getElementById("modal-inputs");
 
-    const routesResponse = await fetch(`/routes`);
+    const routesResponse = await fetch("/routes");
+    const routes = validateJSON(routesResponse);
+
+
+}
+
+
+
+
+function validateJSON(response: Response) {
+    if (response.ok) {
+        return response.json();
+    } else {
+        return Promise.reject(response);
+    }
 }
