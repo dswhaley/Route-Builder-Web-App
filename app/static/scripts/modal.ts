fae0eaabf51a0b6d46e3d9f7e3f86878d4c41db0
dswhaley <whaleyds22@gcc.edu>
2025-12-15 16:03:16 -0500

    Modal Update

--- app/static/scripts/modal.ts
+++ app/static/scripts/modal.ts
@@ -1,12 +1,32 @@
-document.addEventListener("click", activateModal);
+export interface Routes {
+    "coord_string": string,
+    "distance": number,
+    "image_name": string,
+    "rid": number,
+    "route_name": string
+}
+
+export interface RouteList{
+    data: undefined | {routes: Array<Routes>};
+}
+const activityLink = document.getElementById("activity-button")
+activityLink.addEventListener("click", activateModal);
 
 async function activateModal(event: MouseEvent){
   
     const modal = <HTMLInputElement> document.getElementById("create-activity-modal");
     const modalInputsDiv = <HTMLElement> document.getElementById("modal-inputs");
 
-    const routesResponse = await fetch("/routes");
-    const routes = validateJSON(routesResponse);
+    const routesResponse = await fetch("/routes_json");
+    const routes = await validateJSON(routesResponse);
+
+    const routeSelectField = document.getElementById("routes");
+
+    for(let route of routes){
+        const option = document.createElement("option");
+        option.value = route.route_name;
+        routeSelectField.appendChild(option);
+    }
 
 
 }
