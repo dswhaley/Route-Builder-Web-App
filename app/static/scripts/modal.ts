a5f2dd52d778a2ff4ec25b1550dd9b9192ab0264
JMWolverine <147189390+JMWolverine@users.noreply.github.com>
2025-12-15 23:36:26 -0500

    AJAX

--- app/static/scripts/modal.ts
+++ app/static/scripts/modal.ts
@@ -12,8 +12,10 @@ export type RouteList = Array<Route>;
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
-    const modalElement = document.getElementById("formModal");
-    modalElement.addEventListener('show.bs.modal', activateModal);
+    // const modalElement = document.getElementById("formModal");
+    // modalElement.addEventListener('show.bs.modal', activateModal);
+    const activity = document.getElementById("activity-button");
+    activity.addEventListener("click", createModal);
 
     const routeSelectField = document.getElementById("routes");
     routeSelectField.addEventListener('change', handleRouteSelection);
@@ -23,7 +25,143 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 });
 
-async function activateModal(event: MouseEvent){
+async function createModal(){
+    const startingModal = document.getElementById("formModal");
+    const a = document.createElement("div");
+    a.classList.add("modal-dialog");
+    startingModal.appendChild(a);
+    const b = document.createElement("div");
+    b.classList.add("modal-content");
+    a.appendChild(b);
+    const c = document.createElement("div");
+    c.classList.add("modal-header");
+    b.appendChild(c);
+    const d = document.createElement("h1");
+    d.classList.add("modal-title");
+    d.classList.add("fs-5");
+    d.id = "formModalLabel";
+    d.innerText = "Create Activity";
+    c.appendChild(d);
+    const e = document.createElement("button");
+    e.type = "button";
+    e.classList.add("btn-close");
+    e.setAttribute("data-bs-dismiss", "modal");
+    e.setAttribute("aria-label", "Cancel");
+    c.append(e);
+    //header is closed, last time to append to c
+    const f = document.createElement("div");
+    f.classList.add("modal-body");
+    f.classList.add("text-align-center");
+    b.appendChild(f);
+    const g = document.createElement("div");
+    g.id = "modal-inputs";
+    f.appendChild(g);
+    //Activity Title things
+    const activityLabel = document.createElement("label");
+    activityLabel.setAttribute("for", "title");
+    activityLabel.classList.add("form-label");
+    activityLabel.innerText = "Activity Title";
+    g.appendChild(activityLabel);
+    const titleInput = document.createElement("input");
+    titleInput.type = "text";
+    titleInput.id = "title";
+    titleInput.classList.add("form-control");
+    g.appendChild(titleInput); 
+    //Activity Type Things
+    const typeLabel = document.createElement("label");
+    typeLabel.setAttribute("for", "type");
+    typeLabel.classList.add("form-label");
+    typeLabel.innerText = "Activity Type";
+    g.appendChild(typeLabel);
+    const typeInput = document.createElement("select");
+    titleInput.required;
+    titleInput.id = "type";
+    titleInput.classList.add("form-control");
+    const opt1 = document.createElement("option");
+    opt1.value = "";
+    opt1.disabled;
+    opt1.selected;
+    opt1.innerText = "Select Activity Type";
+    titleInput.appendChild(opt1);
+    const opt2 = document.createElement("option");
+    opt2.value = "Run";
+    opt2.innerText = "Run";
+    titleInput.appendChild(opt2);
+    const opt3 = document.createElement("option");
+    opt3.value = "Ride";
+    opt3.innerText = "Ride";
+    titleInput.appendChild(opt3);
+    g.appendChild(typeInput); 
+    //Route things
+    const routeLabel = document.createElement("label");
+    routeLabel.setAttribute("for", "routes");
+    routeLabel.classList.add("form-label");
+    routeLabel.innerText = "Route";
+    g.appendChild(routeLabel);
+    const routeInput = document.createElement("input");
+    routeInput.id = "routes";
+    routeInput.classList.add("form-control");
+    g.appendChild(routeInput);
+
+    //Time things
+    const timeLabel = document.createElement("label");
+    timeLabel.setAttribute("for", "start-time");
+    timeLabel.classList.add("form-label");
+    timeLabel.innerText = "StartTime";
+    g.appendChild(timeLabel);
+    const timeInput = document.createElement("input");
+    timeInput.type = "datetime-local";
+    routeInput.id = "start-time";
+    routeInput.classList.add("form-control");
+    g.appendChild(timeInput);
+
+    //Duration things
+    const durationLabel = document.createElement("label");
+    durationLabel.setAttribute("for", "duration");
+    durationLabel.classList.add("form-label");
+    durationLabel.innerText = "Duration (minutes)";
+    g.appendChild(durationLabel);
+    const durationInput = document.createElement("input");
+    durationInput.type = "number";
+    durationInput.id = "duration";
+    durationInput.classList.add("form-control");
+    g.appendChild(durationInput);
+
+    //Distance things
+    const distanceLabel = document.createElement("label");
+    distanceLabel.setAttribute("for", "distance");
+    distanceLabel.classList.add("form-label");
+    distanceLabel.innerText = "Distance (miles)";
+    g.appendChild(distanceLabel);
+    const distanceInput = document.createElement("input");
+    durationInput.type = "number";
+    durationInput.id = "distance";
+    durationInput.classList.add("form-control");
+    g.appendChild(distanceInput);
+
+    //Footer - append to f
+    const h = document.createElement("div");
+    h.classList.add("modal-footer");
+    f.appendChild(h);
+    const i = document.createElement("button")
+    i.type = "button";
+    i.classList.add("btn");
+    i.classList.add("btn-secondary");
+    i.setAttribute("data-bs-dismiss","modal");
+    i.innerText = "Close";
+    h.appendChild(i);
+    const j = document.createElement("button")
+    j.type = "button";
+    j.classList.add("btn");
+    j.classList.add("btn-primary");
+    j.id = "post-button";
+    i.innerText = "Save Activity";
+    h.appendChild(j);
+
+    activateModal();
+}
+
+async function activateModal(){
     clearActivityForm();
     
     const modal = <HTMLInputElement> document.getElementById("create-activity-modal");
